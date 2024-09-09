8734148cf0625dbdfe318284e01661873e42614b
owais568 <owaishassan697@gmail.com>
2024-09-09 18:35:27 +0500

    add files

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ alert("welcome to js land");//js builtin function
 
 
 alert("please enter valid password");
+alert("welcome to js land..."+"<br>"+"Happy coding");
+alert("welcome to js land...");
+alert("Happy coding"+"<br>"+"_prevent this page from creating additional dialoge");
+alert("jhone doe");
+alert("15 years old");
+alert("Certified mobile application devlopment");
+alert("PIZZA"+"<br>"+"PIZZ"+"<br>"+"PIZ"+"<br>"+"PI"+"<br>"+"P");
+alert("My email address is example@example.com");
+alert("I am trying to learn from the book a samrter way to lear javascript");
+document.write("yah! i can write HTML content through Javascript");
